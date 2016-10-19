4ca01fce45c2d45d267ab08bbd6f4f1dc5896cfe
Nicolás López <nicolaslopezj@me.com>
2016-10-19 13:24:39 -0300

    export all in apollo-accounts

--- apollo-accounts/src/index.js
+++ apollo-accounts/src/index.js
@@ -1,9 +1,32 @@
 import SchemaTypes from './Auth.graphql'
 import SchemaMutations from './Mutation.graphql'
 import Resolvers from './Resolvers'
+import callMethod from './callMethod'
+
+/**
+ * Resolvers
+ */
+import loginWithPassword from './Resolvers/loginWithPassword'
+import logout from './Resolvers/logout'
+import changePassword from './Resolvers/changePassword'
+import createUser from './Resolvers/createUser'
+import verifyEmail from './Resolvers/verifyEmail'
+import resendVerificationEmail from './Resolvers/resendVerificationEmail'
+import forgotPassword from './Resolvers/forgotPassword'
+import resetPassword from './Resolvers/resetPassword'
 
 export {
   SchemaTypes,
   SchemaMutations,
-  Resolvers
+  Resolvers,
+  callMethod,
+  /* Also export all resolvers */
+  loginWithPassword,
+  logout,
+  changePassword,
+  createUser,
+  verifyEmail,
+  resendVerificationEmail,
+  forgotPassword,
+  resetPassword
 }
