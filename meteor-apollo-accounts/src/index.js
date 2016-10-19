26a5bcbeef92bcfc9621c92a7042628366220a44
Janik Vonrotz <login@janikvonrotz.ch>
2016-10-18 17:48:03 -0700

    Update index.js

--- meteor-apollo-accounts/src/index.js
+++ meteor-apollo-accounts/src/index.js
@@ -1,13 +1,13 @@
-﻿import changePassword from 'changePassword'
-import createUser from 'createUser'
-import forgotPassword from 'forgotPassword'
-import getLoginToken from 'getLoginToken'
-import hashPassword from 'hashPassword'
-import loginWithPassword from 'loginWithPassword'
-import logout from 'logout'
-import resendVerificationEmail from 'resendVerificationEmail'
-import resetPassword from 'resetPassword'
-import verifyEmail from 'verifyEmail'
+﻿import changePassword from 'changePassword';
+import createUser from 'createUser';
+import forgotPassword from 'forgotPassword';
+import getLoginToken from 'getLoginToken';
+import hashPassword from 'hashPassword';
+import loginWithPassword from 'loginWithPassword';
+import logout from 'logout';
+import resendVerificationEmail from 'resendVerificationEmail';
+import resetPassword from 'resetPassword';
+import verifyEmail from 'verifyEmail';
 
 
 export { changePassword, createUser, forgotPassword, getLoginToken, hashPassword, loginWithPassword, logout, resendVerificationEmail, resetPassword, verifyEmail }
