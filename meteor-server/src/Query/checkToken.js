926184b6bee0abe026d3a27aaa2af0f011f1d18e
hammadj <jutt@ualberta.ca>
2017-01-16 19:15:08 -0700

    add date check to checkToken

--- meteor-server/src/Query/checkToken.js
+++ meteor-server/src/Query/checkToken.js
@@ -1,13 +1,30 @@
 import {Accounts} from 'meteor/accounts-base'
 import {Meteor} from 'meteor/meteor'
 
-export default async function (root, { token }, {userId}) {
-  const user = Meteor.users.findOne({
-    _id: userId,
-    'services.resume.loginTokens.hashedToken' : Accounts._hashLoginToken(token)
+export default async function (root, { token }, context) {
+  let userId = null;
+
+  const user = await Meteor.users.findOne({
+    _id: context.userId,
+    'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+  }, {
+    fields: {
+      _id: 1,
+      'services.resume.loginTokens.$': 1,
+    },
   });
+
+  if (user) {
+    const loginToken = user.services.resume.loginTokens[0];
+    const expiresAt = Accounts._tokenExpiration(loginToken.when);
+    const isExpired = expiresAt < new Date();
+
+    if (!isExpired) {
+      userId = user._id;
+    }
+  }
   return {
-    success: !!user,
-    userId: user._id || null
+    success: !!userId,
+    userId
   };
 }
