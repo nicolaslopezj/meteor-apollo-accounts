8bb6c020784686f129a2d1531d76dc6da9383aad
Nicolás López Jullian <nicolaslopezj@me.com>
2017-04-27 14:23:25 -0300

    Update checkNpm.js

--- meteor-server/src/checkNpm.js
+++ meteor-server/src/checkNpm.js
@@ -2,4 +2,4 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions'
 
 checkNpmVersions({
   'graphql-loader': '1.0.x'
-}, 'orionsoft:stripe-graphql')
+}, 'nicolaslopezj:apollo-accounts')
